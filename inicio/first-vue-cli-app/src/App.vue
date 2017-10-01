5df967e49e60607a2709b94efcba70237fc517ca
rodrigo rincon <rodrigo.rincon@altran-ais.com.br>
2017-10-01 16:12:37 -0300

    first app with vue-cli

---
+++ inicio/first-vue-cli-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <message>this is a generic message</message>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+  import Message from "./components/message.vue"
+  import Counter from "./components/counter.vue"
+
+  export default {
+    name: 'app',
+    components: {
+      Message,
+      Counter
+    },
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ inicio/first-vue-cli-app/src/components/counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>The count is: {{count}}</h2>
+	<button @click="count+=1">Incremente</button>
+  </div>
+</template>
+
+<script>
+  export default {
+  	data(){
+  		return {
+  			count: 0
+  		}
+  	}
+  }
+</script>
+
+<style>
+</style>
