83966a0b67f13835f8e4583b5057b855f744d379
rodrigo rincon <rodrigo.rincon@altran-ais.com.br>
2017-10-01 16:57:22 -0300

    finalizado projeto 1

--- first-sys-poster-shop/public/script.js
+++ first-sys-poster-shop/public/script.js
@@ -1,1 +1,90 @@
-console.log('It works.');
+
+var app = new Vue({
+	el: "#app",
+	data: {
+		LIMIT: 10,
+		items: [],
+		cart:[],
+		search: "all",
+		lastSearch: "",
+		loading: false,
+		paginateInfos: {
+			init: 0,
+			ending: 0,
+			total: 0
+		}
+	},
+	mounted: function(){
+		this.searchObj()
+	},
+	methods:{
+		addToChart: function(item_id){
+			found = false
+			this.cart.forEach(item=>{
+				if(item.id === item_id){
+					found = true
+					item.quantity++
+					this.cart.splice();
+				}
+			})
+			if(!found){
+				my_item = this.items.filter(obj=>obj.id==item_id)[0]
+				my_item.quantity = 1
+				this.cart.push( my_item )
+			}
+		},
+		mountUrlSearch: function(initial_posix, final_posix, text_search){
+			route = "/search/".concat(text_search)
+			return route.concat("?init=").concat(initial_posix).concat("&ending=").concat(final_posix)
+		},
+		paginateSearch: function(initial_posix, text_search){			
+			this.items = []
+			this.loading = true
+			axios.get( this.mountUrlSearch(initial_posix, initial_posix+this.LIMIT, text_search) ).then(response=>{
+				this.paginateInfos.init = initial_posix
+				this.paginateInfos.ending = initial_posix+response.data.data.length
+				this.paginateInfos.total = response.data.total
+
+				this.items = response.data.data
+				this.lastSearch = text_search
+				this.loading = false
+			}).catch(error=>{
+				console.log(error)
+			})
+		},
+		searchObj: function(){
+			if( this.search.length )
+				this.paginateSearch(0, this.search)
+		},
+		increment: function(item){
+			item.quantity++
+			this.cart.splice();
+		},
+		decrement: function(item){
+			item.quantity--
+			this.cart.splice();
+			if(item.quantity <= 0){
+				for(var i=0; i<this.cart.length; i++){
+					if(this.cart[i].id === item.id){
+						this.cart.splice(i,1)
+						break
+					}
+				}
+			}
+		}
+	},
+	computed:{
+		total(){
+			return this.cart.reduce((sum,item)=>{return sum+=item.price*item.quantity},0)
+		},
+		numberPagesArray(){
+			total_pages = Math.ceil(this.paginateInfos.total/this.LIMIT)
+			return [...Array(total_pages).keys()];
+		}
+	},
+	filters: {
+		formatPrice: function(price){
+			return "R$".concat( price.toFixed(2).replace(".",",") )
+		}
+	}
+})
--- first-sys-poster-shop/server.js
+++ first-sys-poster-shop/server.js
@@ -23,13 +23,24 @@ app.get('/search/:query', function(req, res) {
   const url = 'gallery/search/top/0/?' + querystring.stringify({ q: req.params.query });
   instance.get(url)
     .then(function (result) {
-      res.send(result.data.data.filter(item => !item.is_album && !item.nsfw && !item.animated));
+      results = result.data.data.filter(item => !item.is_album && !item.nsfw && !item.animated)
+      
+      var total_results = results.length
+      if (req.query.init !== undefined && req.query.ending !== undefined){
+        results = results.slice(req.query.init, req.query.ending)
+      }
+
+      results.forEach(function(element) {
+        element.price = 10.00
+      });
+      res.send( {total: total_results, data: results} );
     })
     .catch(function (error) {
       console.log(error);
     })
   ;
 });
+
 
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
