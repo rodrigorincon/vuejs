22b32c5098ad3f1b7cdd16b168d7b2253a808fb3
rodrigo rincon <rodrigo.rincon@altran-ais.com.br>
2018-08-24 17:56:52 -0300

    primeiro componente hardcoded no projeto do cinema

--- vuejs-cinema/src/main.js
+++ vuejs-cinema/src/main.js
@@ -3,7 +3,25 @@ import './style.scss'
 
 new Vue({
   el: "#app",
-  data: {
-
+  components: {
+  	'movie-list': {
+  		template: `<div id="movie-list">
+  					<div v-for="movie in movies">{{ movie.title }}</div>
+  				   </div>`,
+  		data(){ // OU data: function(){
+  			return {
+  				movies: [
+  					{title: "Pulp Fiction"},
+  					{title: "Home ALono"},
+  					{title: "Austin Powers"},
+  				]
+  			}
+  		}
+  	},
+  	'movie-filter': {
+  		template: `<div id="movie-filter">
+  					Movie Filter
+  				   </div>`
+  	}
   }
 })
